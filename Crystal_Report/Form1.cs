373741bc749df4ad91048967d138b8c63032b4ed
Dilip-Sarvaiya <dsarvaiya248@rku.ac.in>
2020-10-26 17:04:39 +0530

    Crystal Report with Database

---
+++ Crystal_Report/Form1.cs
@@ -0,0 +1,30 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crystal_Report
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+  
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReportDocument report = new ReportDocument();
+            report.Load(@"D:\5th_sem_.NET\Crystal_Report\CrystalReport1.rpt");
+            crystalReportViewer1.ReportSource = report;
+            crystalReportViewer1.Refresh();
+        }
+    }
+}
