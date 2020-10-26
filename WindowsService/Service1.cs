cd69e6d5e4ab8b0b0c2f015fde547d75b188d0fa
Dilip-Sarvaiya <dsarvaiya248@rku.ac.in>
2020-10-26 17:05:12 +0530

    Windows Service Demonstration

---
+++ WindowsService/Service1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt");
+        }
+
+        protected override void OnStop()
+        {
+            System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStop.txt");
+        }
+    }
+}
