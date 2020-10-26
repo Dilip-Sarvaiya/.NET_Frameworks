a96bf204665882fa0f4e03c135f90c35b8eee827
Dilip-Sarvaiya <dsarvaiya248@rku.ac.in>
2020-10-26 17:00:18 +0530

    Types of Parameters

---
+++ Parameters/Program.cs
@@ -0,0 +1,28 @@
+﻿ using System;
+
+namespace Parameters
+{
+
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numbers are....");
+            int a = 4, b = 5;
+            int[] array = { 1, 2, 3, 4, 5 };
+            add_product(a, b);
+            add_product(a, b, array);
+        }
+        public static void add_product(int value1, int value2, params int[] extra)
+        {
+            if (extra != null)
+            {
+                foreach (int i in extra)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
