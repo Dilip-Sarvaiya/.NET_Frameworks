85eab66df45a7c60e6037249887499922273c7f0
Dilip-Sarvaiya <dsarvaiya248@rku.ac.in>
2020-10-26 17:07:30 +0530

    WPF With StackPanel,WrapPanel,DockPanel,Canvas,Styles

---
+++ WPF_Second/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace WPF_Second
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
