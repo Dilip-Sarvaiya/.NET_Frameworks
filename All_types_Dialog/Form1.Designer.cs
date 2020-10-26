d113a11788cd5f35e7f3c61c5cbf6887efcc6d23
Dilip-Sarvaiya <dsarvaiya248@rku.ac.in>
2020-10-26 17:01:06 +0530

    All types of Dialog Controls

---
+++ All_types_Dialog/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace All_types_Dialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.foreColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.user_txt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.file_data = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label5 = new System.Windows.Forms.Label();
+            this.file_lbl = new System.Windows.Forms.Label();
+            this.folder_path = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.folder_files = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.fontToolStripMenuItem,
+            this.colorToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveFileToolStripMenuItem,
+            this.openFolderToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(851, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(52, 24);
+            this.fontToolStripMenuItem.Text = "Font";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.foreColorToolStripMenuItem,
+            this.backColorToolStripMenuItem});
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.colorToolStripMenuItem.Text = "Color";
+            // 
+            // foreColorToolStripMenuItem
+            // 
+            this.foreColorToolStripMenuItem.Name = "foreColorToolStripMenuItem";
+            this.foreColorToolStripMenuItem.Size = new System.Drawing.Size(163, 26);
+            this.foreColorToolStripMenuItem.Text = "Fore Color";
+            this.foreColorToolStripMenuItem.Click += new System.EventHandler(this.foreColorToolStripMenuItem_Click);
+            // 
+            // backColorToolStripMenuItem
+            // 
+            this.backColorToolStripMenuItem.Name = "backColorToolStripMenuItem";
+            this.backColorToolStripMenuItem.Size = new System.Drawing.Size(163, 26);
+            this.backColorToolStripMenuItem.Text = "Back Color";
+            this.backColorToolStripMenuItem.Click += new System.EventHandler(this.backColorToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(86, 24);
+            this.openToolStripMenuItem.Text = "Open File";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveFileToolStripMenuItem
+            // 
+            this.saveFileToolStripMenuItem.Name = "saveFileToolStripMenuItem";
+            this.saveFileToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.saveFileToolStripMenuItem.Text = "Save File";
+            this.saveFileToolStripMenuItem.Click += new System.EventHandler(this.saveFileToolStripMenuItem_Click);
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(105, 24);
+            this.openFolderToolStripMenuItem.Text = "Open Folder";
+            this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.openFolderToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 1;
+            // 
+            // user_txt
+            // 
+            this.user_txt.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.user_txt.Location = new System.Drawing.Point(244, 158);
+            this.user_txt.Name = "user_txt";
+            this.user_txt.Size = new System.Drawing.Size(179, 22);
+            this.user_txt.TabIndex = 2;
+            this.user_txt.KeyUp += new System.Windows.Forms.KeyEventHandler(this.user_txt_KeyUp);
+            this.user_txt.MouseHover += new System.EventHandler(this.user_txt_MouseHover);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(118, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "File Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(106, 342);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "File Content";
+            // 
+            // file_data
+            // 
+            this.file_data.Location = new System.Drawing.Point(234, 337);
+            this.file_data.Name = "file_data";
+            this.file_data.Size = new System.Drawing.Size(381, 22);
+            this.file_data.TabIndex = 7;
+            this.file_data.MouseEnter += new System.EventHandler(this.file_data_MouseEnter);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(108, 395);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Folder Path";
+            // 
+            // file_lbl
+            // 
+            this.file_lbl.AutoSize = true;
+            this.file_lbl.Location = new System.Drawing.Point(268, 278);
+            this.file_lbl.Name = "file_lbl";
+            this.file_lbl.Size = new System.Drawing.Size(71, 17);
+            this.file_lbl.TabIndex = 4;
+            this.file_lbl.Text = "File Name";
+            // 
+            // folder_path
+            // 
+            this.folder_path.AutoSize = true;
+            this.folder_path.Location = new System.Drawing.Point(258, 395);
+            this.folder_path.Name = "folder_path";
+            this.folder_path.Size = new System.Drawing.Size(71, 17);
+            this.folder_path.TabIndex = 9;
+            this.folder_path.Text = "File Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(108, 442);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Folder Files";
+            // 
+            // folder_files
+            // 
+            this.folder_files.Location = new System.Drawing.Point(234, 442);
+            this.folder_files.Name = "folder_files";
+            this.folder_files.Size = new System.Drawing.Size(517, 22);
+            this.folder_files.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 506);
+            this.Controls.Add(this.folder_files);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.folder_path);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.file_data);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.file_lbl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.user_txt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem foreColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveFileToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox user_txt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox file_data;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label file_lbl;
+        private System.Windows.Forms.Label folder_path;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox folder_files;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ All_types_Dialog/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace All_types_Dialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void foreColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            DialogResult dialogResult= colorDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                label1.ForeColor = colorDialog.Color;
+            }
+        }
+
+        private void backColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+
+            DialogResult dialogResult = colorDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                label1.BackColor = colorDialog.Color;
+            }
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            //FontDialog fontDialog = new FontDialog();
+            DialogResult dialogResult = fontDialog1.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                label1.Font = fontDialog1.Font;
+            }
+        }
+
+        private void user_txt_KeyUp(object sender, KeyEventArgs e)
+        {
+            label1.Text = user_txt.Text;
+        }
+
+        private void user_txt_MouseHover(object sender, EventArgs e)
+        {
+            user_txt.Text = "";
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Text Files(*.txt)|*.txt|Pictures(*.jpg)|*.jpg|PDF Files (*.pdf)|*.pdf|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog1.Multiselect = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //file_lbl.Text = openFileDialog1.FileNames;
+                foreach (var filename in openFileDialog1.FileNames)
+                {
+                    file_lbl.Text += "\n" + filename;
+                }
+                StreamReader streamReader = new StreamReader(openFileDialog1.FileName);
+                file_data.Text = streamReader.ReadToEnd();
+                streamReader.Dispose();
+            }
+        }
+
+        private void saveFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title="Save your file";
+            saveFileDialog1.Filter = "Text Files(*.txt)|*.txt|Pictures(*.jpg)|*.jpg|PDF Files (*.pdf)|*.pdf|All Files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 3;
+            saveFileDialog1.DefaultExt = ".txt";
+            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if(saveFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                StreamWriter streamWriter = new StreamWriter(saveFileDialog1.FileName);
+                streamWriter.Write(file_data.Text);
+                streamWriter.Dispose();
+                this.Text = saveFileDialog1.FileName;
+            }
+        
+        }
+
+        private void openFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+         
+            //folderBrowserDialog1.ShowDialog();
+            //folderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyDocuments;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                folder_path.Text = folderBrowserDialog1.SelectedPath;
+                string [] files=Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+                foreach (var file in files)
+                {
+                    folder_files.Text += file+" ";
+                }
+            }
+        }
+
+        private void file_data_MouseEnter(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(file_data,"Contents of the File");
+        }
+    }
+}
